ec2edb6f558f54a10cc46b819dee02ab8268e09d
Dane Juras <verydanejuras@gmail.com>
2018-02-04 19:28:15 -0500

    adding QueryStore

---
+++ QueryStore.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AniDroid.AniList
+{
+    internal class QueryStore
+    {
+        public const string GetSeriesByIdAndType = @"query ($id: Int!, $type: MediaType) {
+  Media(id: $id, type: $type) {
+    id
+    title {
+      romaji
+      english
+      native
+      userPreferred
+    }
+    startDate {
+      year
+      month
+      day
+    }
+    endDate {
+      year
+      month
+      day
+    }
+    coverImage {
+      large
+      medium
+    }
+    bannerImage
+    format
+    type
+    status
+    episodes
+    chapters
+    volumes
+    season
+    description
+    averageScore
+    meanScore
+    genres
+    synonyms
+    nextAiringEpisode {
+      airingAt
+      timeUntilAiring
+      episode
+    }
+  }
+}";
+
+    }
+}
