96b8d64e3c5bccb85ba114a9ee4d4b57328f42a4
Dane Juras <verydanejuras@gmail.com>
2018-02-12 10:45:35 -0500

    removing default values from interfaces, added paging methods to service interface

--- AniDroid.AniList/Interfaces/IAniListService.cs
+++ AniDroid.AniList/Interfaces/IAniListService.cs
@@ -16,52 +16,73 @@ namespace AniDroid.AniList.Interfaces
 
         #region Media
 
-        Task<IAniListServiceResponse<Media>> GetMedia(int id, Media.MediaType type, CancellationToken cToken = default(CancellationToken));
-        Task<IAniListServiceResponse<AniListObject.PagedData<List<Media>>>> SearchMedia(string queryText, int page, int count, Media.MediaType type = null, CancellationToken cToken = default(CancellationToken));
+        Task<IAniListServiceResponse<Media>> GetMedia(int id, Media.MediaType type, CancellationToken cToken);
+
+        Task<IAniListServiceResponse<AniListObject.PagedData<List<Media>>>> SearchMedia(string queryText, int page, int count, Media.MediaType type, CancellationToken cToken);
+
+        IAsyncEnumerable<AniListObject.PagedData<ICollection<Media>>> SearchMediaPaging(string queryText, Media.MediaType type, int perPage);
 
         #endregion
 
         #region User
 
-        Task<IAniListServiceResponse<User>> GetUser(string name, CancellationToken cToken = default(CancellationToken));
-        Task<IAniListServiceResponse<Media.MediaListCollection>> GetUserMediaList(string userName, Media.MediaType type, CancellationToken cToken = default(CancellationToken));
-        Task<IAniListServiceResponse<AniListObject.PagedData<List<User>>>> SearchUsers(string queryText, int page, int count, CancellationToken cToken = default(CancellationToken));
+        Task<IAniListServiceResponse<User>> GetUser(string name, CancellationToken cToken);
+
+        Task<IAniListServiceResponse<Media.MediaListCollection>> GetUserMediaList(string userName, Media.MediaType type, CancellationToken cToken);
+
+        Task<IAniListServiceResponse<AniListObject.PagedData<List<User>>>> SearchUsers(string queryText, int page, int count, CancellationToken cToken);
+
+        IAsyncEnumerable<AniListObject.PagedData<ICollection<User>>> SearchUsersPaging(string queryText, int perPage);
 
         #endregion
 
         #region Activity
 
-        Task<IAniListServiceResponse<AniListObject.PagedData<List<AniListActivity>>>> GetAniListActivity(int page, int count, CancellationToken cToken = default(CancellationToken));
-        Task<IAniListServiceResponse<AniListActivity>> PostTextActivity(string text, CancellationToken cToken = default(CancellationToken));
-        Task<IAniListServiceResponse<AniListActivity.ActivityReply>> PostActivityReply(int activityId, string text, CancellationToken cToken = default(CancellationToken));
-        Task<IAniListServiceResponse<List<User>>> ToggleLike(int id, AniListObject.LikeableType type, CancellationToken cToken = default(CancellationToken));
-        Task<IAniListServiceResponse<AniListActivity>> GetAniListActivityById(int id, CancellationToken cToken = default(CancellationToken));
-        Task<IAniListServiceResponse<AniListObject.PagedData<List<AniListNotification>>>> GetAniListNotifications(int page, int count, CancellationToken cToken = default(CancellationToken));
+        Task<IAniListServiceResponse<AniListObject.PagedData<List<AniListActivity>>>> GetAniListActivity(int page, int count, CancellationToken cToken);
+
+        Task<IAniListServiceResponse<AniListActivity>> PostTextActivity(string text, CancellationToken cToken);
+
+        Task<IAniListServiceResponse<AniListActivity.ActivityReply>> PostActivityReply(int activityId, string text, CancellationToken cToken);
+
+        Task<IAniListServiceResponse<List<User>>> ToggleLike(int id, AniListObject.LikeableType type, CancellationToken cToken);
+
+        Task<IAniListServiceResponse<AniListActivity>> GetAniListActivityById(int id, CancellationToken cToken);
+
+        Task<IAniListServiceResponse<AniListObject.PagedData<List<AniListNotification>>>> GetAniListNotifications(int page, int count, CancellationToken cToken);
 
         #endregion
 
         #region Character
 
-        Task<IAniListServiceResponse<AniListObject.PagedData<List<Character>>>> SearchCharacters(string queryText, int page, int count, CancellationToken cToken = default(CancellationToken));
-        Task<IAniListServiceResponse<Character>> GetCharacterById(int id, CancellationToken cToken = default(CancellationToken));
+        Task<IAniListServiceResponse<AniListObject.PagedData<List<Character>>>> SearchCharacters(string queryText, int page, int count, CancellationToken cToken);
+
+        IAsyncEnumerable<AniListObject.PagedData<ICollection<Character>>> SearchCharactersPaging(string queryText, int perPage);
+
+        Task<IAniListServiceResponse<Character>> GetCharacterById(int id, CancellationToken cToken);
 
         #endregion
 
         #region Staff
 
-        Task<IAniListServiceResponse<AniListObject.PagedData<List<Staff>>>> SearchStaff(string queryText, int page, int count, CancellationToken cToken = default(CancellationToken));
+        Task<IAniListServiceResponse<AniListObject.PagedData<List<Staff>>>> SearchStaff(string queryText, int page, int count, CancellationToken cToken);
+
+        IAsyncEnumerable<AniListObject.PagedData<ICollection<Staff>>> SearchStaffPaging(string queryText, int perPage);
 
         #endregion
 
         #region Studio
 
-        Task<IAniListServiceResponse<AniListObject.PagedData<List<Studio>>>> SearchStudios(string queryText, int page, int count, CancellationToken cToken = default(CancellationToken));
+        Task<IAniListServiceResponse<AniListObject.PagedData<List<Studio>>>> SearchStudios(string queryText, int page, int count, CancellationToken cToken);
+
+        IAsyncEnumerable<AniListObject.PagedData<ICollection<Studio>>> SearchStudiosPaging(string queryText, int perPage);
 
         #endregion
 
         #region ForumThread
 
-        Task<IAniListServiceResponse<AniListObject.PagedData<List<ForumThread>>>> SearchForumThreads(string queryText, int page, int count, CancellationToken cToken = default(CancellationToken));
+        Task<IAniListServiceResponse<AniListObject.PagedData<List<ForumThread>>>> SearchForumThreads(string queryText, int page, int count, CancellationToken cToken);
+
+        IAsyncEnumerable<AniListObject.PagedData<ICollection<ForumThread>>> SearchForumThreadsPaging(string queryText, int perPage);
 
         #endregion
 
