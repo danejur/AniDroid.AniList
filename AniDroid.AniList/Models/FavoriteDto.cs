8ae65c0fc6f7643c1b57b9df5500a12f0d85f426
Unknown <nikey646@hotmail.com>
2018-02-21 20:54:33 +1100

    Added FavoriteDto.HasValue
    With a cheeky Json.Net attribute ;)

--- AniDroid.AniList/Models/FavoriteDto.cs
+++ AniDroid.AniList/Models/FavoriteDto.cs
@@ -6,5 +6,9 @@
         public int? MangaId { get; set; }
         public int? CharacterId { get; set; }
         public int? StaffId { get; set; }
+
+        [Newtonsoft.Json.JsonIgnore]
+        public bool HasValue
+            => AnimeId.HasValue && MangaId.HasValue && CharacterId.HasValue && StaffId.HasValue;
     } // TODO: Add Missing Id
 }
