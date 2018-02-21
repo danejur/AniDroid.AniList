52b3960aa6790d6edc511da0058c99f3ba416647
Unknown <nikey646@hotmail.com>
2018-02-21 20:49:41 +1100

    Concept for doing the generic favourite toggle method.

---
+++ AniDroid.AniList/Models/FavoriteDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AniDroid.AniList.Models
+{
+    public class FavoriteDto
+    {
+        public int? AnimeId { get; set; }
+        public int? MangaId { get; set; }
+        public int? CharacterId { get; set; }
+        public int? StaffId { get; set; }
+    } // TODO: Add Missing Id
+}
