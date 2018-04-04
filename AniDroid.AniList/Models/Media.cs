66c6f167373b9855ba9014f1958f684116063fed
Dane Juras <verydanejuras@gmail.com>
2018-04-04 12:23:01 -0400

    adding method for checking if media has valid stats

--- AniDroid.AniList/Models/Media.cs
+++ AniDroid.AniList/Models/Media.cs
@@ -207,6 +207,12 @@ namespace AniDroid.AniList.Models
             public List<AniListScoreDistribution> ScoreDistribution { get; set; }
             public List<AniListStatusDistribution> StatusDistribution { get; set; }
             public List<MediaAiringProgression> AiringProgression { get; set; }
+
+            public bool AreStatsValid()
+            {
+                return ScoreDistribution?.Count(x => x.Amount > 0) >= 3 || AiringProgression?.Count >= 3 ||
+                       StatusDistribution?.Any(x => x.Amount >= 3) == true;
+            }
         }
 
         public class MediaAiringProgression
