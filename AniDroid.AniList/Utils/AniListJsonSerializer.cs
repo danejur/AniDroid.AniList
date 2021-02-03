46cf4be61b21597f43e3c10c3b30ec0251e3ef29
Dane Juras <verydanejuras@gmail.com>
2021-02-03 16:18:26 -0500

    removing restsharp dependency

--- AniDroid.AniList/Utils/AniListJsonSerializer.cs
+++ AniDroid.AniList/Utils/AniListJsonSerializer.cs
@@ -1,23 +1,18 @@
 ﻿using System.IO;
 using AniDroid.AniList.Utils.Internal;
 using Newtonsoft.Json;
-using RestSharp;
-using RestSharp.Deserializers;
-using RestSharp.Serializers;
 
 namespace AniDroid.AniList.Utils
 {
-    public class AniListJsonSerializer : ISerializer, IDeserializer
+    public class AniListJsonSerializer
     {
-        public string DateFormat { get; set; }
-        public string RootElement { get; set; }
         public string Namespace { get; set; }
         public string ContentType { get; set; }
-        public Newtonsoft.Json.JsonSerializer Serializer { get; }
+        public JsonSerializer Serializer { get; }
 
         public AniListJsonSerializer()
         {
-            Serializer = new Newtonsoft.Json.JsonSerializer
+            Serializer = new JsonSerializer
             {
                 MissingMemberHandling = MissingMemberHandling.Ignore,
                 NullValueHandling = NullValueHandling.Ignore,
@@ -27,42 +22,21 @@ namespace AniDroid.AniList.Utils
             ContentType = "application/json";
         }
 
-        public AniListJsonSerializer(Newtonsoft.Json.JsonSerializer serializer)
-        {
-            Serializer = serializer;
-            ContentType = "application/json";
-        }
-
         public string Serialize(object obj)
         {
-            using (var stringWriter = new StringWriter())
-            using (var jsonTextWriter = new JsonTextWriter(stringWriter))
-            {
-                Serializer.Serialize(jsonTextWriter, obj);
-                return stringWriter.ToString();
-            }
-        }
-
-        public T Deserialize<T>(IRestResponse response)
-        {
-            var content = response.Content;
-
-            using (var stringReader = new StringReader(content))
-            using (var jsonTextReader = new JsonTextReader(stringReader))
-            {
-                return Serializer.Deserialize<T>(jsonTextReader);
-            }
+            using var stringWriter = new StringWriter();
+            using var jsonTextWriter = new JsonTextWriter(stringWriter);
+            Serializer.Serialize(jsonTextWriter, obj);
+            return stringWriter.ToString();
         }
 
         public T Deserialize<T>(string content)
         {
-            using (var stringReader = new StringReader(content))
-            using (var jsonTextReader = new JsonTextReader(stringReader))
-            {
-                return Serializer.Deserialize<T>(jsonTextReader);
-            }
+            using var stringReader = new StringReader(content);
+            using var jsonTextReader = new JsonTextReader(stringReader);
+            return Serializer.Deserialize<T>(jsonTextReader);
         }
 
-        public static AniListJsonSerializer Default => new AniListJsonSerializer();
+        public static AniListJsonSerializer Default => new();
     }
 }
